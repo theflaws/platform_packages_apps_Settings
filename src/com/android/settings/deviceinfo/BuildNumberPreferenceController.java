5465bf6a34aea646d2d010c394fa6848337e0021
Tiny @ngel <safe.nysocho305@outlook.com>
2022-09-26 03:49:24 -0400

    Disable Build Number click for developer options

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -105,6 +105,7 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public boolean handlePreferenceTreeClick(Preference preference) {
+        return false;
         if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
             return false;
         }
